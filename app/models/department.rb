f2d807e28975bd87051295d8b405dd7775615088
julian <julianrawcliffe@gmail.com>
2014-08-11 21:20:23 +0100

    Changed the HABTM reference.

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,6 +2,8 @@ class Department < ActiveRecord::Base
 
   belongs_to :sub_department
   has_one :category
+  #has_many :department_extension_ranges
+  #has_many :extension_ranges, :through => :department_extension_ranges, :foreign_key => "extension_range_id", :primary_key => "id"
   has_and_belongs_to_many :extension_ranges, :join_table => :departments_extension_ranges
 
   attr_reader :extension_range_tokens
