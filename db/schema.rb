938d604369135b11418345935720237fb3832076
julian <julianrawcliffe@gmail.com>
2014-08-11 22:20:32 +0100

    Updated to include policies

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 18) do
+ActiveRecord::Schema.define(version: 20140809163722) do
+
+  create_table "Phonebook_SourceData", id: false, force: true do |t|
+    t.integer "LBSNO",                     null: false
+    t.string  "Surname",        limit: 30
+    t.string  "Knownname",      limit: 30
+    t.string  "UID",            limit: 50, null: false
+    t.string  "Dptcode",        limit: 10
+    t.string  "Department"
+    t.string  "PhoneExtension", limit: 10
+    t.string  "Room",           limit: 20
+  end
+
+  create_table "SchDBtoPhonebook_SourceData", id: false, force: true do |t|
+    t.integer "LBSNO",                null: false
+    t.string  "Surname",   limit: 30
+    t.string  "Knownname", limit: 30
+    t.string  "UID",       limit: 50, null: false
+  end
 
   create_table "activity_logs", force: true do |t|
-    t.string   "item_type",  limit: 64, default: "Object",              null: false
-    t.integer  "item_id",                                               null: false
-    t.string   "act_action",            default: "update"
-    t.string   "updated_by", limit: 32, default: "Anonymous"
+    t.string   "item_type"
+    t.integer  "item_id"
+    t.string   "act_action"
+    t.string   "updated_by"
     t.text     "activity"
-    t.datetime "act_tstamp",            default: '2014-01-25 16:57:31'
+    t.datetime "act_tstamp"
+  end
+
+  create_table "archiving_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 7,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "building_floors", force: true do |t|
@@ -29,25 +56,51 @@ ActiveRecord::Schema.define(version: 18) do
   end
 
   create_table "buildings", force: true do |t|
-    t.string   "name",       limit: 32, default: "Building", null: false
+    t.string   "name",       limit: 32, default: "LBS building", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "categories", force: true do |t|
-    t.string   "name",       limit: 10, default: "Staff", null: false
+    t.string   "name",       limit: 50, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "client_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 11,       null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "client_version_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 4,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "conference_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 3,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "departments", force: true do |t|
-    t.string   "name",                      limit: 32,                null: false
-    t.integer  "category_id",               limit: 1,                 null: false
-    t.boolean  "active",                               default: true
+    t.string   "name",            limit: 50,                null: false
+    t.integer  "category_id",                default: 1,    null: false
+    t.string   "department_code", limit: 4,  default: "00"
+    t.integer  "active",          limit: 1,  default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "department_code",           limit: 4,  default: "00", null: false
-    t.integer  "default_dialling_right_id",            default: 1
   end
 
   create_table "departments_extension_ranges", id: false, force: true do |t|
@@ -55,9 +108,11 @@ ActiveRecord::Schema.define(version: 18) do
     t.integer "extension_range_id", null: false
   end
 
-  create_table "dialling_rights", force: true do |t|
-    t.string   "name",        limit: 32, null: false
+  create_table "dial_plan_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
     t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 1,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -70,19 +125,91 @@ ActiveRecord::Schema.define(version: 18) do
   end
 
   create_table "extensions", force: true do |t|
-    t.integer  "extension",  limit: 8, null: false
+    t.string   "extension",  limit: 10, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_table "external_access_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 6,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "location_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 8,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "mobility_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 9,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "persist_chat_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 10,       null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "phonebook_sessions", force: true do |t|
+    t.string   "session_id",             null: false
+    t.text     "phonebook_session_data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "phonebook_sessions", ["session_id"], name: "index_phonebook_sessions_on_session_id", unique: true
+  add_index "phonebook_sessions", ["updated_at"], name: "index_phonebook_sessions_on_updated_at"
+
   create_table "phones", force: true do |t|
-    t.string   "firstname",         limit: 32, null: false
-    t.string   "surname",           limit: 32, null: false
-    t.string   "uid",               limit: 32, null: false
-    t.integer  "sub_department_id", limit: 1
-    t.integer  "extension_id"
-    t.integer  "room_id"
-    t.integer  "dialling_right_id", limit: 1
+    t.string   "firstname",                 limit: 32, default: "Firstname", null: false
+    t.string   "surname",                   limit: 32, default: "Surname",   null: false
+    t.string   "uid",                       limit: 32,                       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "sub_department_id",         limit: 2
+    t.integer  "extension_id",              limit: 2
+    t.integer  "room_id",                   limit: 2
+    t.integer  "dial_plan_policy_id",       limit: 2,  default: 1,           null: false
+    t.integer  "voice_policy_id",           limit: 2,  default: 1,           null: false
+    t.integer  "conferencing_policy_id",    limit: 2,  default: 1,           null: false
+    t.integer  "external_access_policy_id", limit: 2,  default: 1,           null: false
+    t.integer  "archiving_policy_id",       limit: 2,  default: 1,           null: false
+    t.integer  "client_version_policy_id",  limit: 2,  default: 1,           null: false
+    t.integer  "pin_policy_id",             limit: 2,  default: 1,           null: false
+    t.integer  "location_policy_id",        limit: 2,  default: 1,           null: false
+    t.integer  "mobility_policy_id",        limit: 2,  default: 1,           null: false
+    t.integer  "persist_chat_policy_id",    limit: 2,  default: 1,           null: false
+    t.integer  "client_policy_id",          limit: 2,  default: 1,           null: false
+  end
+
+  create_table "pin_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 5,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "policy_types", force: true do |t|
+    t.string   "name",        default: "Policy type", null: false
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -95,9 +222,6 @@ ActiveRecord::Schema.define(version: 18) do
     t.datetime "updated_at"
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
-
   create_table "room_statuses", force: true do |t|
     t.string   "name",       limit: 16, default: "Public", null: false
     t.datetime "created_at"
@@ -105,53 +229,71 @@ ActiveRecord::Schema.define(version: 18) do
   end
 
   create_table "rooms", force: true do |t|
-    t.string   "name",              limit: 32, default: "room",         null: false
+    t.string   "name",              limit: 32, default: "LBS room",         null: false
     t.string   "public_name",       limit: 32, default: "Public room name", null: false
-    t.integer  "room_status_id",               default: 1,                  null: false
-    t.integer  "building_id",                                               null: false
-    t.integer  "building_floor_id",            default: 1,                  null: false
+    t.integer  "room_status_id",    limit: 2,  default: 1,                  null: false
+    t.integer  "building_id",       limit: 2,                               null: false
+    t.integer  "building_floor_id", limit: 2,  default: 1,                  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "sub_department_extension_range_view", id: false, force: true do |t|
-    t.integer "sub_department_id",             default: 0, null: false
-    t.string  "sub_department",     limit: 64,             null: false
-    t.integer "department_id",                 default: 0, null: false
-    t.string  "department",         limit: 32,             null: false
-    t.integer "extension_range_id", limit: 8,  default: 0, null: false
-    t.integer "first_extension",                           null: false
-    t.integer "last_extension",                            null: false
+  create_table "rooms_backup", id: false, force: true do |t|
+    t.integer  "id",                limit: 2,  null: false
+    t.string   "name",              limit: 32, null: false
+    t.string   "public_name",       limit: 32, null: false
+    t.integer  "room_status_id",    limit: 2,  null: false
+    t.integer  "building_id",       limit: 2,  null: false
+    t.integer  "building_floor_id", limit: 2,  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "sub_departments", force: true do |t|
-    t.string   "name",                         limit: 64,                null: false
-    t.integer  "department_id",                limit: 2,                 null: false
+    t.string   "name",                         limit: 50,                null: false
+    t.string   "sub_department_code",          limit: 4,  default: "00"
+    t.integer  "department_id"
+    t.integer  "preferred_extension_range_id"
+    t.integer  "active",                       limit: 1,  default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "sub_department_code",          limit: 4,  default: "00", null: false
-    t.integer  "preferred_extension_range_id"
+    t.integer  "dial_plan_policy_id",          limit: 2,  default: 1,    null: false
+    t.integer  "voice_policy_id",              limit: 2,  default: 1,    null: false
+    t.integer  "conferencing_policy_id",       limit: 2,  default: 1,    null: false
+    t.integer  "external_access_policy_id",    limit: 2,  default: 1,    null: false
+    t.integer  "archiving_policy_id",          limit: 2,  default: 1,    null: false
+    t.integer  "client_version_policy_id",     limit: 2,  default: 1,    null: false
+    t.integer  "pin_policy_id",                limit: 2,  default: 1,    null: false
+    t.integer  "location_policy_id",           limit: 2,  default: 1,    null: false
+    t.integer  "mobility_policy_id",           limit: 2,  default: 1,    null: false
+    t.integer  "persist_chat_policy_id",       limit: 2,  default: 1,    null: false
+    t.integer  "client_policy_id",             limit: 2,  default: 1,    null: false
   end
 
   create_table "users", force: true do |t|
     t.string   "username",                        null: false
-    t.datetime "remember_created_at"
     t.integer  "sign_in_count",       default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "users_roles", id: false, force: true do |t|
     t.integer "user_id"
     t.integer "role_id"
   end
 
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
+  create_table "voice_policies", force: true do |t|
+    t.string   "name",                       default: "Global", null: false
+    t.text     "description"
+    t.integer  "policy_type_id",   limit: 2, default: 2,        null: false
+    t.string   "lync_policy_name",           default: "Global", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
