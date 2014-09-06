1618efc7f1fee2da53100751db13a19f9738669f
julian <julianrawcliffe@gmail.com>
2014-09-06 18:43:59 +0100

    Added an additional format for use with timestamps

--- config/initializers/date_format.rb
+++ config/initializers/date_format.rb
@@ -1,2 +1,4 @@
 Date::DATE_FORMATS.merge!(
   :my_date => lambda { |time| time.strftime("#{ActiveSupport::Inflector.ordinalize(time.day)} %b %Y") })
+# The above won't work with datetime columns, so use the following
+Time::DATE_FORMATS.merge!(:time2date => lambda { |time| time.strftime("#{ActiveSupport::Inflector.ordinalize(time.day)} %b %Y") })
